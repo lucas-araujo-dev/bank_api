d4c247377f80b1b50d1d03fd8fa73bf8ec1caf51
lucas araujo <lucas.trabalho.araujo@gmail.com>
2024-05-23 14:42:22 -0300

    feat: added setter of the value with the rate and validation of the value with the rate not being greater than the account balance

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,22 +1,30 @@
 class Transaction < ApplicationRecord
   belongs_to :account
 
-  enum payment_type: { credit: 'C', debit: 'D', pix: 'P' }
+  enum payment_type: { C: 0, D: 1, P: 2 }
 
   with_options presence: true do
     validates :value, numericality: { greater_than: 0 }
-    validates :account, :payment_type
+    validates :account, :payment_type, :value_with_tax
   end
-
-  before_commit :set_value_with_tax
 
   validate :value_must_be_less_than_balance
 
+  before_validation :set_value_with_tax
+
   private
+
+  def set_value_with_tax
+    return if value.blank? || payment_type.blank?
+
+    tax_calculator_class = TaxCalculatorFactory.create(payment_type)
+    self.value_with_tax = tax_calculator_class.calculate_value_with_tax(value)
+  end
 
   def value_must_be_less_than_balance
     return if account.nil?
+    return if value_with_tax.nil? || value_with_tax < account.balance
 
-    errors.add(:value, 'cannot be greater than balance')
+    errors.add(:value_with_tax, 'cannot be greater than balance')
   end
 end
